48950b1e843ce2d4f09d8ac6fa6687eac6156d1b
Verina Ong Dinata <verina.od1997@gmail.com>
2022-07-25 21:55:26 +0800

    add CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # read all
+  get 'tasks', to: 'tasks#index'
+
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # read specific task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
