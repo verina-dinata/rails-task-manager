f0f82537e47e78b471dccb32ed1b1ec54ce7c84d
Verina Ong Dinata <verina.od1997@gmail.com>
2022-07-27 20:22:19 +0800

    Update edit operations & refactor code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -18,26 +17,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    # raise
-
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
 
-  # read all
-  get 'tasks', to: 'tasks#index'
+  # # read all
+  # get 'tasks', to: 'tasks#index'
 
-  # create
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # # create
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  # read specific task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # read specific task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
+  # # update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # delete
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
